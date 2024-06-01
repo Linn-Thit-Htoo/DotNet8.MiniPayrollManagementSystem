59244867db7e161201a4f77ab9f14f04c0efcb73
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 00:44:42 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Features/Payroll/PayrollController.cs
+++ DotNet8.MiniPayrollManagementSystem/Features/Payroll/PayrollController.cs
@@ -1,73 +1,72 @@
 ﻿using DotNet8.MiniPayrollManagementSystem.Models.Setup.Payroll;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Features.Payroll
+namespace DotNet8.MiniPayrollManagementSystem.Api.Features.Payroll;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PayrollController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PayrollController : BaseController
+    private readonly BL_Payroll _bL_Payroll;
+
+    public PayrollController(BL_Payroll bL_Payroll)
     {
-        private readonly BL_Payroll _bL_Payroll;
+        _bL_Payroll = bL_Payroll;
+    }
 
-        public PayrollController(BL_Payroll bL_Payroll)
+    [HttpGet]
+    public async Task<IActionResult> GetPayrollListByEmployee(string employeeCode, string? fromDate = "", string? toDate = "")
+    {
+        try
         {
-            _bL_Payroll = bL_Payroll;
+            var lst = await _bL_Payroll.GetPayrollByEmployeeAsync(employeeCode, fromDate, toDate);
+            return Content(lst);
         }
-
-        [HttpGet]
-        public async Task<IActionResult> GetPayrollListByEmployee(string employeeCode, string? fromDate = "", string? toDate = "")
+        catch (Exception ex)
         {
-            try
-            {
-                var lst = await _bL_Payroll.GetPayrollByEmployeeAsync(employeeCode, fromDate, toDate);
-                return Content(lst);
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return InternalServerError(ex);
         }
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreatePayroll([FromBody] PayrollRequestModel requestModel)
+    [HttpPost]
+    public async Task<IActionResult> CreatePayroll([FromBody] PayrollRequestModel requestModel)
+    {
+        try
         {
-            try
-            {
-                int result = await _bL_Payroll.CreatePayrollAsync(requestModel);
-                return result > 0 ? Created("Payroll Created.") : BadRequest("Creating Fail.");
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            int result = await _bL_Payroll.CreatePayrollAsync(requestModel);
+            return result > 0 ? Created("Payroll Created.") : BadRequest("Creating Fail.");
         }
-
-        [HttpPatch("{pId}")]
-        public async Task<IActionResult> UpdatePayroll([FromBody] PayrollRequestModel requestModel, string pId)
+        catch (Exception ex)
         {
-            try
-            {
-                int result = await _bL_Payroll.UpdatePayrollAsync(requestModel, pId);
-                return result > 0 ? Accepted("Payroll Updated.") : BadRequest("Updating Fail.");
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return InternalServerError(ex);
         }
+    }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePayroll(string id)
+    [HttpPatch("{pId}")]
+    public async Task<IActionResult> UpdatePayroll([FromBody] PayrollRequestModel requestModel, string pId)
+    {
+        try
         {
-            try
-            {
-                int result = await _bL_Payroll.DeletePayrollAsync(id);
-                return result > 0 ? Accepted("Payroll Deleted.") : BadRequest("Deleting Fail.");
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            int result = await _bL_Payroll.UpdatePayrollAsync(requestModel, pId);
+            return result > 0 ? Accepted("Payroll Updated.") : BadRequest("Updating Fail.");
+        }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePayroll(string id)
+    {
+        try
+        {
+            int result = await _bL_Payroll.DeletePayrollAsync(id);
+            return result > 0 ? Accepted("Payroll Deleted.") : BadRequest("Deleting Fail.");
+        }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
         }
     }
 }
