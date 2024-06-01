51e9625bbe6fd7f85273898a04df9c9b978fba77
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-01 16:40:13 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Services/ModularService.cs
+++ DotNet8.MiniPayrollManagementSystem/Services/ModularService.cs
@@ -18,6 +18,9 @@ public static class ModularService
         return services;
     }
 
+    #region MyRegion
+
+    #endregion
     private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
     {
         services.AddScoped<BL_Employee>();
