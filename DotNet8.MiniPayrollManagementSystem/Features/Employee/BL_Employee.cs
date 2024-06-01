8e3600d5781e7eec6be5cccf341c703eecb3d02d
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-01 16:36:39 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Features/Employee/BL_Employee.cs
+++ DotNet8.MiniPayrollManagementSystem/Features/Employee/BL_Employee.cs
@@ -1,20 +1,19 @@
 ﻿using DotNet8.MiniPayrollManagementSystem.Features.Employee;
 using DotNet8.MiniPayrollManagementSystem.Models.Setup.Employee;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Features.Employee
+namespace DotNet8.MiniPayrollManagementSystem.Api.Features.Employee;
+
+public class BL_Employee
 {
-    public class BL_Employee
+    private readonly DA_Employee _dA_Employee;
+
+    public BL_Employee(DA_Employee dA_Employee)
     {
-        private readonly DA_Employee _dA_Employee;
+        _dA_Employee = dA_Employee;
+    }
 
-        public BL_Employee(DA_Employee dA_Employee)
-        {
-            _dA_Employee = dA_Employee;
-        }
-
-        public async Task<EmployeeListResponseModel> GetEmployeeListAsync()
-        {
-            return await _dA_Employee.GetEmployeeListAsync();
-        }
+    public async Task<EmployeeListResponseModel> GetEmployeeListAsync()
+    {
+        return await _dA_Employee.GetEmployeeListAsync();
     }
 }
