16f4a122eeefaf8c37e82fc1994f5cda07795f69
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-01 16:45:50 +0630

    -

--- DotNet8.MiniPayrollManagementSystem.Models/Setup/Employee/EmployeeModel.cs
+++ DotNet8.MiniPayrollManagementSystem.Models/Setup/Employee/EmployeeModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.MiniPayrollManagementSystem.Models.Setup.Employee
+﻿namespace DotNet8.MiniPayrollManagementSystem.Models.Setup.Employee
 {
     public class EmployeeModel
     {
