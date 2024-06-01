260bf269266e653af12ffef2616da017c41bdb6c
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 00:50:27 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Commands/Employee/DeleteEmployee/DeleteEmployeeCommandHandler.cs
+++ DotNet8.MiniPayrollManagementSystem/Commands/Employee/DeleteEmployee/DeleteEmployeeCommandHandler.cs
@@ -1,20 +1,19 @@
 ﻿using DotNet8.MiniPayrollManagementSystem.Api.Repositories.Employee;
 using MediatR;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Commands.Employee.DeleteEmployee
+namespace DotNet8.MiniPayrollManagementSystem.Api.Commands.Employee.DeleteEmployee;
+
+public class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand, int>
 {
-    public class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand, int>
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public DeleteEmployeeCommandHandler(IEmployeeRepository employeeRepository)
     {
-        private readonly IEmployeeRepository _employeeRepository;
+        _employeeRepository = employeeRepository;
+    }
 
-        public DeleteEmployeeCommandHandler(IEmployeeRepository employeeRepository)
-        {
-            _employeeRepository = employeeRepository;
-        }
-
-        public async Task<int> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
-        {
-            return await _employeeRepository.DeleteEmployeeAsync(request.EmployeeId);
-        }
+    public async Task<int> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        return await _employeeRepository.DeleteEmployeeAsync(request.EmployeeId);
     }
 }
