95343d601831cf179b32e9066761c9216e27220e
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-01 16:46:02 +0630

    -

--- DotNet8.MiniPayrollManagementSystem.Models/Setup/Employee/EmployeeModel.cs
+++ DotNet8.MiniPayrollManagementSystem.Models/Setup/Employee/EmployeeModel.cs
@@ -1,15 +1,14 @@
-﻿namespace DotNet8.MiniPayrollManagementSystem.Models.Setup.Employee
+﻿namespace DotNet8.MiniPayrollManagementSystem.Models.Setup.Employee;
+
+public class EmployeeModel
 {
-    public class EmployeeModel
-    {
-        public long EmployeeId { get; set; }
-        public string EmployeeCode { get; set; } = null!;
-        public string EmployeeName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string PhonNumber { get; set; } = null!;
-        public string HireDate { get; set; } = null!;
-        public string Position { get; set; } = null!;
-        public decimal Salary { get; set; }
-        public bool IsActive { get; set; }
-    }
+    public long EmployeeId { get; set; }
+    public string EmployeeCode { get; set; } = null!;
+    public string EmployeeName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhonNumber { get; set; } = null!;
+    public string HireDate { get; set; } = null!;
+    public string Position { get; set; } = null!;
+    public decimal Salary { get; set; }
+    public bool IsActive { get; set; }
 }
