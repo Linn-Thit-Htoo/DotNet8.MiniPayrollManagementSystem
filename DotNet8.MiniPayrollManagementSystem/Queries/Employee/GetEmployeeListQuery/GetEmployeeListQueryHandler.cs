7a58a620ed51095c2a617290c599f37b28ba3521
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-01 16:38:36 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Queries/Employee/GetEmployeeListQuery/GetEmployeeListQueryHandler.cs
+++ DotNet8.MiniPayrollManagementSystem/Queries/Employee/GetEmployeeListQuery/GetEmployeeListQueryHandler.cs
@@ -2,20 +2,19 @@
 using DotNet8.MiniPayrollManagementSystem.Repositories.Employee;
 using MediatR;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Queries.Employee.GetEmployeeListQuery
+namespace DotNet8.MiniPayrollManagementSystem.Api.Queries.Employee.GetEmployeeListQuery;
+
+public class GetEmployeeListQueryHandler : IRequestHandler<GetEmployeeListQuery, EmployeeListResponseModel>
 {
-    public class GetEmployeeListQueryHandler : IRequestHandler<GetEmployeeListQuery, EmployeeListResponseModel>
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public GetEmployeeListQueryHandler(IEmployeeRepository employeeRepository)
     {
-        private readonly IEmployeeRepository _employeeRepository;
+        _employeeRepository = employeeRepository;
+    }
 
-        public GetEmployeeListQueryHandler(IEmployeeRepository employeeRepository)
-        {
-            _employeeRepository = employeeRepository;
-        }
-
-        public async Task<EmployeeListResponseModel> Handle(GetEmployeeListQuery request, CancellationToken cancellationToken)
-        {
-            return await _employeeRepository.GetEmployeeListAsync();
-        }
+    public async Task<EmployeeListResponseModel> Handle(GetEmployeeListQuery request, CancellationToken cancellationToken)
+    {
+        return await _employeeRepository.GetEmployeeListAsync();
     }
 }
