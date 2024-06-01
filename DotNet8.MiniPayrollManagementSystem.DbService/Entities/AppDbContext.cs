f2cc284edd2a1f29bd86344e00b146f7b57813af
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-01 15:53:34 +0630

    Add project files.

---
+++ DotNet8.MiniPayrollManagementSystem.DbService/Entities/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.MiniPayrollManagementSystem.DbService.Entities;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblEmployee> TblEmployees { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblEmployee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId);
+
+            entity.ToTable("Tbl_Employee");
+
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.EmployeeCode).HasMaxLength(50);
+            entity.Property(e => e.EmployeeName).HasMaxLength(50);
+            entity.Property(e => e.HireDate).HasMaxLength(50);
+            entity.Property(e => e.PhonNumber).HasMaxLength(50);
+            entity.Property(e => e.Position).HasMaxLength(50);
+            entity.Property(e => e.Salary).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DotNet8.MiniPayrollManagementSystem.DbService/Entities/TblEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.MiniPayrollManagementSystem.DbService.Entities;
+
+public partial class TblEmployee
+{
+    public long EmployeeId { get; set; }
+
+    public string EmployeeCode { get; set; } = null!;
+
+    public string EmployeeName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PhonNumber { get; set; } = null!;
+
+    public byte[] HireDate { get; set; } = null!;
+
+    public string Position { get; set; } = null!;
+
+    public decimal Salary { get; set; }
+
+    public bool IsActive { get; set; }
+}
