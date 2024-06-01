a15f6d5547c41e0fa32e7c99a08a1ab4853518e6
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 00:50:42 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Commands/Employee/UpdateEmployee/UpdateEmployeeCommand.cs
+++ DotNet8.MiniPayrollManagementSystem/Commands/Employee/UpdateEmployee/UpdateEmployeeCommand.cs
@@ -1,11 +1,10 @@
 ﻿using DotNet8.MiniPayrollManagementSystem.Models.Setup.Employee;
 using MediatR;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Commands.Employee.UpdateEmployee
+namespace DotNet8.MiniPayrollManagementSystem.Api.Commands.Employee.UpdateEmployee;
+
+public class UpdateEmployeeCommand : IRequest<int>
 {
-    public class UpdateEmployeeCommand : IRequest<int>
-    {
-        public EmployeeRequestModel EmployeeRequestModel { get; set; }
-        public long EmployeeId { get; set; }
-    }
+    public EmployeeRequestModel EmployeeRequestModel { get; set; }
+    public long EmployeeId { get; set; }
 }
