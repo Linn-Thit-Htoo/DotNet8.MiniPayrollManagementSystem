4094bd4b7a3b76ed511b4455234f45c7c6c74dae
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 00:53:14 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Repositories/Payroll/IPayrollRepository.cs
+++ DotNet8.MiniPayrollManagementSystem/Repositories/Payroll/IPayrollRepository.cs
@@ -1,12 +1,11 @@
 ﻿using DotNet8.MiniPayrollManagementSystem.Models.Setup.Payroll;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Repositories.Payroll
+namespace DotNet8.MiniPayrollManagementSystem.Api.Repositories.Payroll;
+
+public interface IPayrollRepository
 {
-    public interface IPayrollRepository
-    {
-        Task<IEnumerable<PayrollResponseModel>> GetPayrollListByEmployeeAsync(string employeeCode, string fromDate, string toDate);
-        Task<int> CreatePayrollAsync(PayrollRequestModel requestModel);
-        Task<int> DeletePayrollAsync(string pId);
-        Task<int> UpdatePayrollAsync(PayrollRequestModel requestModel, string pId);
-    }
+    Task<IEnumerable<PayrollResponseModel>> GetPayrollListByEmployeeAsync(string employeeCode, string fromDate, string toDate);
+    Task<int> CreatePayrollAsync(PayrollRequestModel requestModel);
+    Task<int> DeletePayrollAsync(string pId);
+    Task<int> UpdatePayrollAsync(PayrollRequestModel requestModel, string pId);
 }
