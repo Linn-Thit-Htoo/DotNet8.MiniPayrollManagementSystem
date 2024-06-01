cb7ea215bbb47048b29422781d77da1189250ca7
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-01 16:38:47 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Repositories/Employee/EmployeeRepository.cs
+++ DotNet8.MiniPayrollManagementSystem/Repositories/Employee/EmployeeRepository.cs
@@ -1,9 +1,10 @@
 ﻿using DotNet8.MiniPayrollManagementSystem.DbService.Entities;
 using DotNet8.MiniPayrollManagementSystem.Models;
 using DotNet8.MiniPayrollManagementSystem.Models.Setup.Employee;
+using DotNet8.MiniPayrollManagementSystem.Repositories.Employee;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.MiniPayrollManagementSystem.Repositories.Employee
+namespace DotNet8.MiniPayrollManagementSystem.Api.Repositories.Employee
 {
     public class EmployeeRepository : IEmployeeRepository
     {
--- DotNet8.MiniPayrollManagementSystem/Services/ModularService.cs
+++ DotNet8.MiniPayrollManagementSystem/Services/ModularService.cs
@@ -1,4 +1,5 @@
 ﻿using DotNet8.MiniPayrollManagementSystem.Api.Features.Employee;
+using DotNet8.MiniPayrollManagementSystem.Api.Repositories.Employee;
 using DotNet8.MiniPayrollManagementSystem.DbService.Entities;
 using DotNet8.MiniPayrollManagementSystem.Repositories.Employee;
 using Microsoft.EntityFrameworkCore;
