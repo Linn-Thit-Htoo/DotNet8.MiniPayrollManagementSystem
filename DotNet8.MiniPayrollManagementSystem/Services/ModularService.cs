7a2a79b9fdb7c22ea8cf2c6eb89b174cd17f5f4b
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-01 16:40:03 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Services/ModularService.cs
+++ DotNet8.MiniPayrollManagementSystem/Services/ModularService.cs
@@ -3,62 +3,61 @@ using DotNet8.MiniPayrollManagementSystem.Api.Repositories.Employee;
 using DotNet8.MiniPayrollManagementSystem.DbService.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Services
+namespace DotNet8.MiniPayrollManagementSystem.Api.Services;
+
+public static class ModularService
 {
-    public static class ModularService
+    public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            services.AddDbContextServices(builder);
-            services.AddMediatRService();
-            services.AddDataAccessServices();
-            services.AddBusinessLogicServices();
-            services.AddRepositoryServices();
-            services.AddJsonServices();
-            return services;
-        }
+        services.AddDbContextServices(builder);
+        services.AddMediatRService();
+        services.AddDataAccessServices();
+        services.AddBusinessLogicServices();
+        services.AddRepositoryServices();
+        services.AddJsonServices();
+        return services;
+    }
 
-        private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
-        {
-            services.AddScoped<BL_Employee>();
-            return services;
-        }
+    private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    {
+        services.AddScoped<BL_Employee>();
+        return services;
+    }
 
-        private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
-        {
-            services.AddScoped<DA_Employee>();
-            return services;
-        }
+    private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    {
+        services.AddScoped<DA_Employee>();
+        return services;
+    }
 
-        private static IServiceCollection AddDbContextServices(this IServiceCollection services, WebApplicationBuilder builder)
+    private static IServiceCollection AddDbContextServices(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient);
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+    private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+    {
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        return services;
+    }
+
+    private static IServiceCollection AddJsonServices(this IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(opt =>
         {
-            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-            return services;
-        }
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+        return services;
+    }
 
-        private static IServiceCollection AddJsonServices(this IServiceCollection services)
-        {
-            services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
-            return services;
-        }
-
-        private static IServiceCollection AddMediatRService(this IServiceCollection services)
-        {
-            services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Program).Assembly));
-            return services;
-        }
+    private static IServiceCollection AddMediatRService(this IServiceCollection services)
+    {
+        services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Program).Assembly));
+        return services;
     }
 }
