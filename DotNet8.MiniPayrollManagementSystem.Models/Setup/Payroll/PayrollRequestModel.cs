c84deff46a72127ad3d649eacff69b98f9184680
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 01:07:16 +0630

    -

--- DotNet8.MiniPayrollManagementSystem.Models/Setup/Payroll/PayrollRequestModel.cs
+++ DotNet8.MiniPayrollManagementSystem.Models/Setup/Payroll/PayrollRequestModel.cs
@@ -1,13 +1,12 @@
-﻿namespace DotNet8.MiniPayrollManagementSystem.Models.Setup.Payroll
+﻿namespace DotNet8.MiniPayrollManagementSystem.Models.Setup.Payroll;
+
+public class PayrollRequestModel
 {
-    public class PayrollRequestModel
-    {
-        public string? EmployeeName { get; set; }
-        public string? PayDate { get; set; }
-        public decimal GrossPay { get; set; }
-        public decimal NetPay { get; set; }
-        public decimal? DeductionAmount { get; set; }
-        public decimal? BonusAmount { get; set; }
-        public decimal? TaxAmount { get; set; }
-    }
+    public string? EmployeeName { get; set; }
+    public string? PayDate { get; set; }
+    public decimal GrossPay { get; set; }
+    public decimal NetPay { get; set; }
+    public decimal? DeductionAmount { get; set; }
+    public decimal? BonusAmount { get; set; }
+    public decimal? TaxAmount { get; set; }
 }
