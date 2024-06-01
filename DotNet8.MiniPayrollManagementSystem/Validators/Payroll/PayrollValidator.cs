6c5b366d59092733e253777fbaa39de164125a2a
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 01:03:49 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Validators/Payroll/PayrollValidator.cs
+++ DotNet8.MiniPayrollManagementSystem/Validators/Payroll/PayrollValidator.cs
@@ -1,19 +1,18 @@
 ﻿using DotNet8.MiniPayrollManagementSystem.Models.Setup.Payroll;
 using FluentValidation;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Validators.Payroll
+namespace DotNet8.MiniPayrollManagementSystem.Api.Validators.Payroll;
+
+public class PayrollValidator : AbstractValidator<PayrollRequestModel>
 {
-    public class PayrollValidator : AbstractValidator<PayrollRequestModel>
+    public PayrollValidator()
     {
-        public PayrollValidator()
-        {
-            RuleFor(x => x.EmployeeName).NotEmpty().NotNull().WithMessage("Employee Name cannot be empty.");
-            RuleFor(x => x.PayDate).NotEmpty().NotNull().WithMessage("Pay Date cannot be empty.");
-            RuleFor(x => x.GrossPay).NotEmpty().NotNull().WithMessage("Gross Pay cannot be empty.");
-            RuleFor(x => x.NetPay).NotEmpty().NotNull().WithMessage("Net Pay cannot be empty.");
-            //RuleFor(x => x.DeductionAmount).NotEmpty().NotNull().WithMessage("Deduction Amount cannot be empty.");
-            //RuleFor(x => x.BonusAmount).NotEmpty().NotNull().WithMessage("Bonus Amount cannot be empty.");
-            //RuleFor(x => x.TaxAmount).NotEmpty().NotNull().WithMessage("Tax Amount cannot be empty.");
-        }
+        RuleFor(x => x.EmployeeName).NotEmpty().NotNull().WithMessage("Employee Name cannot be empty.");
+        RuleFor(x => x.PayDate).NotEmpty().NotNull().WithMessage("Pay Date cannot be empty.");
+        RuleFor(x => x.GrossPay).NotEmpty().NotNull().WithMessage("Gross Pay cannot be empty.");
+        RuleFor(x => x.NetPay).NotEmpty().NotNull().WithMessage("Net Pay cannot be empty.");
+        //RuleFor(x => x.DeductionAmount).NotEmpty().NotNull().WithMessage("Deduction Amount cannot be empty.");
+        //RuleFor(x => x.BonusAmount).NotEmpty().NotNull().WithMessage("Bonus Amount cannot be empty.");
+        //RuleFor(x => x.TaxAmount).NotEmpty().NotNull().WithMessage("Tax Amount cannot be empty.");
     }
 }
