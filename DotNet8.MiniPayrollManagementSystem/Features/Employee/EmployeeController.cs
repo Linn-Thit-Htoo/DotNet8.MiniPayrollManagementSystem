cdffa124a6019566faaa29284cc73276054ef894
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-01 16:37:28 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Features/Employee/EmployeeController.cs
+++ DotNet8.MiniPayrollManagementSystem/Features/Employee/EmployeeController.cs
@@ -1,30 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Features.Employee
+namespace DotNet8.MiniPayrollManagementSystem.Api.Features.Employee;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmployeeController : ControllerBase
+    private readonly BL_Employee _bL_Employee;
+
+    public EmployeeController(BL_Employee bL_Employee)
     {
-        private readonly BL_Employee _bL_Employee;
+        _bL_Employee = bL_Employee;
+    }
 
-        public EmployeeController(BL_Employee bL_Employee)
+    [HttpGet]
+    public async Task<IActionResult> GetEmployees()
+    {
+        try
         {
-            _bL_Employee = bL_Employee;
+            var lst = await _bL_Employee.GetEmployeeListAsync();
+            return Ok(lst);
         }
-
-        [HttpGet]
-        public async Task<IActionResult> GetEmployees()
+        catch (Exception ex)
         {
-            try
-            {
-                var lst = await _bL_Employee.GetEmployeeListAsync();
-                return Ok(lst);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
         }
     }
 }
