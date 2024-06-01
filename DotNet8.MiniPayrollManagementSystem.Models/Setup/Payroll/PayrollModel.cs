4aa28c9d917d63f35115f59c5d53c4c2d088cbde
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 01:06:55 +0630

    -

--- DotNet8.MiniPayrollManagementSystem.Models/Setup/Payroll/PayrollModel.cs
+++ DotNet8.MiniPayrollManagementSystem.Models/Setup/Payroll/PayrollModel.cs
@@ -1,15 +1,14 @@
-﻿namespace DotNet8.MiniPayrollManagementSystem.Models.Setup.Payroll
+﻿namespace DotNet8.MiniPayrollManagementSystem.Models.Setup.Payroll;
+
+public class PayrollModel
 {
-    public class PayrollModel
-    {
-        public string PId { get; set; } = null!;
-        public string EmployeeName { get; set; } = null!;
-        public string PayDate { get; set; } = null!;
-        public decimal GrossPay { get; set; }
-        public decimal NetPay { get; set; }
-        public decimal? DeductionAmount { get; set; }
-        public decimal? BonusAmount { get; set; }
-        public decimal? TaxAmount { get; set; }
-        public bool IsAcitve { get; set; }
-    }
+    public string PId { get; set; } = null!;
+    public string EmployeeName { get; set; } = null!;
+    public string PayDate { get; set; } = null!;
+    public decimal GrossPay { get; set; }
+    public decimal NetPay { get; set; }
+    public decimal? DeductionAmount { get; set; }
+    public decimal? BonusAmount { get; set; }
+    public decimal? TaxAmount { get; set; }
+    public bool IsAcitve { get; set; }
 }
