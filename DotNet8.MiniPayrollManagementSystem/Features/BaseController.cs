91fd0e37832fa7a243cb5aa47a6306ff6628369c
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 00:46:45 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Features/BaseController.cs
+++ DotNet8.MiniPayrollManagementSystem/Features/BaseController.cs
@@ -2,30 +2,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace DotNet8.MiniPayrollManagementSystem.Api.Features
+namespace DotNet8.MiniPayrollManagementSystem.Api.Features;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object? obj)
     {
-        protected IActionResult Content(object? obj)
-        {
-            return Content(JsonConvert.SerializeObject(obj), "application/json");
-        }
+        return Content(JsonConvert.SerializeObject(obj), "application/json");
+    }
 
-        protected IActionResult InternalServerError(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
+    protected IActionResult InternalServerError(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
+    }
 
-        protected IActionResult Created(string message)
-        {
-            return StatusCode(201, message);
-        }
+    protected IActionResult Created(string message)
+    {
+        return StatusCode(201, message);
+    }
 
-        protected IActionResult Accepted(string message)
-        {
-            return StatusCode(202, message);
-        }
+    protected IActionResult Accepted(string message)
+    {
+        return StatusCode(202, message);
     }
 }
