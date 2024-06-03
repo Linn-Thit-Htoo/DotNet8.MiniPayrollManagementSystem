037f6f6d5e1e08e02f352314994f235d67a528e6
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-03 18:27:55 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Commands/Employee/DeleteEmployee/DeleteEmployeeCommand.cs
+++ DotNet8.MiniPayrollManagementSystem/Commands/Employee/DeleteEmployee/DeleteEmployeeCommand.cs
@@ -2,6 +2,9 @@
 
 namespace DotNet8.MiniPayrollManagementSystem.Api.Commands.Employee.DeleteEmployee;
 
+#region MyRegion
+
+#endregion
 public class DeleteEmployeeCommand : IRequest<int>
 {
     public long EmployeeId { get; set; }
