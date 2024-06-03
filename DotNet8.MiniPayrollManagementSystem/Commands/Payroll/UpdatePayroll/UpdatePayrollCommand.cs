9b51b54d0c94b0aa39bac68afed1a6b25303c6f0
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-03 18:33:15 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Commands/Payroll/UpdatePayroll/UpdatePayrollCommand.cs
+++ DotNet8.MiniPayrollManagementSystem/Commands/Payroll/UpdatePayroll/UpdatePayrollCommand.cs
@@ -5,9 +5,9 @@ namespace DotNet8.MiniPayrollManagementSystem.Api.Commands.Payroll.UpdatePayroll
 
 #region Update Payroll Command
 
-#endregion
 public class UpdatePayrollCommand : IRequest<int>
 {
     public PayrollRequestModel PayrollRequestModel { get; set; }
     public string PId { get; set; } = null!;
 }
+#endregion
