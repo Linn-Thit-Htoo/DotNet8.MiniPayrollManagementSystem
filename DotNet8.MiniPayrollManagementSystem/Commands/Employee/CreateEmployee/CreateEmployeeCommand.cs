f97bcc6285e0768efa55d313a8e612fa8df4b167
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-03 18:26:19 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Commands/Employee/CreateEmployee/CreateEmployeeCommand.cs
+++ DotNet8.MiniPayrollManagementSystem/Commands/Employee/CreateEmployee/CreateEmployeeCommand.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace DotNet8.MiniPayrollManagementSystem.Api.Commands.Employee.CreateEmployee;
 
-#region MyRegion
+#region CreateEmployeeCommand
 
 #endregion
 public class CreateEmployeeCommand : IRequest<int>
