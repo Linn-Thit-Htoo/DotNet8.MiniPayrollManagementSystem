1d2c490788072282089613fa7d11612d4b66b399
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-03 18:31:15 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Commands/Payroll/CreatePayroll/CreatePayrollCommandHandler.cs
+++ DotNet8.MiniPayrollManagementSystem/Commands/Payroll/CreatePayroll/CreatePayrollCommandHandler.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace DotNet8.MiniPayrollManagementSystem.Api.Commands.Payroll.CreatePayroll;
 
-#region MyRegion
+#region CreatePayrollCommandHandler
 
 #endregion
 public class CreatePayrollCommandHandler : IRequestHandler<CreatePayrollCommand, int>
