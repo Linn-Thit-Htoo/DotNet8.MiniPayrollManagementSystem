b28accf6bbbd371e3d5478e6c6c4d450584e02cf
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-03 18:33:42 +0630

    -

--- DotNet8.MiniPayrollManagementSystem/Commands/Payroll/UpdatePayroll/UpdatePayrollCommandHandler.cs
+++ DotNet8.MiniPayrollManagementSystem/Commands/Payroll/UpdatePayroll/UpdatePayrollCommandHandler.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace DotNet8.MiniPayrollManagementSystem.Api.Commands.Payroll.UpdatePayroll;
 
-#region UpdatePayrollCommandHandler
+#region Update Payroll Command Handler
 
 #endregion
 public class UpdatePayrollCommandHandler : IRequestHandler<UpdatePayrollCommand, int>
