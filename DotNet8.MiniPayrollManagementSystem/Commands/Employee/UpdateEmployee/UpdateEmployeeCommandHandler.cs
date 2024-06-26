23451206270ed67bcd2b4b8482f072689e8d1944
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-26 18:46:04 +0630

    Format Code

--- DotNet8.MiniPayrollManagementSystem/Commands/Employee/UpdateEmployee/UpdateEmployeeCommandHandler.cs
+++ DotNet8.MiniPayrollManagementSystem/Commands/Employee/UpdateEmployee/UpdateEmployeeCommandHandler.cs
@@ -11,9 +11,15 @@ public class UpdateEmployeeCommandHandler : IRequestHandler<UpdateEmployeeComman
         _employeeRepository = employeeRepository;
     }
 
-    public async Task<int> Handle(UpdateEmployeeCommand request, CancellationToken cancellationToken)
+    public async Task<int> Handle(
+        UpdateEmployeeCommand request,
+        CancellationToken cancellationToken
+    )
     {
-        return await _employeeRepository.UpdateEmployeeAsync(request.EmployeeRequestModel, request.EmployeeId);
+        return await _employeeRepository.UpdateEmployeeAsync(
+            request.EmployeeRequestModel,
+            request.EmployeeId
+        );
     }
 }
 
