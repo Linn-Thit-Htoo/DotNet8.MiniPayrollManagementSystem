fe18e74fc1219e0ec669142cc70eb693dcfde7c0
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-26 18:41:32 +0630

    [Added] Global Usings in Models

---
+++ DotNet8.MiniPayrollManagementSystem.Models/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using DotNet8.MiniPayrollManagementSystem.DbService.Entities;
+global using DotNet8.MiniPayrollManagementSystem.Models.Setup.Employee;
+global using DotNet8.MiniPayrollManagementSystem.Models.Setup.Payroll;
