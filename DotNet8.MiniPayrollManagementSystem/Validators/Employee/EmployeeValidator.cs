114324d0d9e0038a8e10e25830c249f1c1de18fc
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-26 18:53:41 +0630

    Format Code

--- DotNet8.MiniPayrollManagementSystem/Validators/Employee/EmployeeValidator.cs
+++ DotNet8.MiniPayrollManagementSystem/Validators/Employee/EmployeeValidator.cs
@@ -4,9 +4,16 @@ public class EmployeeValidator : AbstractValidator<EmployeeRequestModel>
 {
     public EmployeeValidator()
     {
-        RuleFor(x => x.EmployeeName).NotNull().NotEmpty().WithMessage("Employee Name cannot be empty.");
+        RuleFor(x => x.EmployeeName)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Employee Name cannot be empty.");
         RuleFor(x => x.Email).NotNull().NotEmpty().EmailAddress().WithMessage("Email is invalid.");
-        RuleFor(x => x.PhoneNumber).NotNull().NotEmpty().MaximumLength(11).WithMessage("Phone Number is invalid.");
+        RuleFor(x => x.PhoneNumber)
+            .NotNull()
+            .NotEmpty()
+            .MaximumLength(11)
+            .WithMessage("Phone Number is invalid.");
         RuleFor(x => x.HireDate).NotNull().NotEmpty().WithMessage("Hire Date cannot be empty.");
         RuleFor(x => x.Position).NotNull().NotEmpty().WithMessage("Position cannot be empty.");
         RuleFor(x => x.Salary).NotNull().NotEmpty().WithMessage("Salary is invalid.");
