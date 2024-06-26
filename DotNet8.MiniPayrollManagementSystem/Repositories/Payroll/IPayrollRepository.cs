e201375a98c33fc85141dd9d86b388b780aea3af
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-26 18:52:38 +0630

    Format Code

--- DotNet8.MiniPayrollManagementSystem/Repositories/Payroll/IPayrollRepository.cs
+++ DotNet8.MiniPayrollManagementSystem/Repositories/Payroll/IPayrollRepository.cs
@@ -2,7 +2,11 @@
 
 public interface IPayrollRepository
 {
-    Task<IEnumerable<PayrollResponseModel>> GetPayrollListByEmployeeAsync(string employeeCode, string fromDate, string toDate);
+    Task<IEnumerable<PayrollResponseModel>> GetPayrollListByEmployeeAsync(
+        string employeeCode,
+        string fromDate,
+        string toDate
+    );
     Task<int> CreatePayrollAsync(PayrollRequestModel requestModel);
     Task<int> DeletePayrollAsync(string pId);
     Task<int> UpdatePayrollAsync(PayrollRequestModel requestModel, string pId);
